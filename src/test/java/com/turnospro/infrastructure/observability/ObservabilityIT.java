cfa69c2649f38f647a1b80a79b55bb1924e39e9d
alapierre <lapierre82@gmail.com>
2026-09-11 19:18:51 -0300

    chore: renombramos los tests de integracion con el sufijo IT

--- src/test/java/com/turnospro/infrastructure/observability/ObservabilityIT.java
+++ src/test/java/com/turnospro/infrastructure/observability/ObservabilityIT.java
@@ -26,7 +26,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-class ObservabilityIntegrationTest extends BaseIntegrationTest {
+class ObservabilityIT extends BaseIntegrationTest {
 
     @Autowired
     private MockMvc mockMvc;
--- src/test/java/com/turnospro/infrastructure/security/MultiTenantSecurityIT.java
+++ src/test/java/com/turnospro/infrastructure/security/MultiTenantSecurityIT.java
@@ -21,7 +21,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-class MultiTenantSecurityIntegrationTest extends BaseIntegrationTest {
+class MultiTenantSecurityIT extends BaseIntegrationTest {
 
     @Autowired
     private MockMvc mockMvc;
